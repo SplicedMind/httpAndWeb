c74b0149890bf71b29a147e21537180997b1e77a
Akinbode Femi <femi661@gmail.com>
2017-01-11 15:55:55 +0100

    Edited the code to run on command prompt

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # httpAndWeb
 A javascript project that consumes a public API using HTTP client library
+
+Install the 'request-promise module' using 'npm install request-promise --save'.
--- app/httpWeb.js
+++ app/httpWeb.js
@@ -1,12 +1,18 @@
-module.exports = {
- 
- httpWeb: function(doma) {
-   var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;  
-   var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.fedger.io/v1/company/"+doma+"/funding/status?apikey=bef4fa9abab04ccccca609874424bb76", false);
-   xhr.send();
+var http = require('http');
+var readline = require('readline');
+var requestPromise = require('request-promise');
+console.log('\nThe API returns the funding status of domain input\n');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('Enter a domain namme:  ', (domain) => {
+    
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;  
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.fedger.io/v1/company/"+domain+"/funding/status?apikey=bef4fa9abab04ccccca609874424bb76", false);
+xhr.send();
 
-   return xhr.responseText;   
- }
-
-}
+console.log(xhr.responseText.replace(/,/g, '\n'));
+    rl.close();
+});
