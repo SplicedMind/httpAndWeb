0ef564fcbd36e455d87cc522c7cc86c1485a5378
Akinbode Femi <femi661@gmail.com>
2017-01-13 09:41:26 +0100

    Added error handling

--- app/httpWeb.js
+++ app/httpWeb.js
@@ -13,6 +13,19 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.fedger.io/v1/company/"+domain+"/funding/status?apikey=bef4fa9abab04ccccca609874424bb76", false);
 xhr.send();
 
-console.log(xhr.responseText.replace(/,/g, '\n'));
+if (xhr.status === 200) 
+{
+	console.log(xhr.responseText.replace(/,/g, '\n'));
     rl.close();
+}
+else if(xhr.status === 404)
+{
+	console.log("Please Enter A Valid Domain");
+    rl.close();
+}
+else
+{
+	console.log("Oops, Something went wrong, please try again later");
+    rl.close();
+}
 });
